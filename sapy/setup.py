d3d965af0e5a5fb51d80815ba33c728c78703a16
cenmartins <carlos.martins@ifc.edu.br>
2017-07-05 05:20:15 -0300

    Delete setup.py

--- sapy/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name="SAPy",
-      packages=["sapy"],
-      package_data={"sapy": ["icons/*.svg"]},
-      classifiers=["Example :: Invalid"],
-      # Declare orangedemo package to contain widgets for the "SAPy" category
-      entry_points={"orange.widgets": "SAPy = sapy"},
-      )
